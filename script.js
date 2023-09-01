5e8565b6f5d7adc170d9ba93646e63e90729b0ce
Amresh Tripathy <amreshtripathy184@gmail.com>
2023-09-01 21:19:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
     //Write your code here
+	 const f = document.querySelector("[name='fname']").value;
+	const l = document.querySelector("[name='lname']").value;
 
+	alert(`${f} ${l}`);
 }
